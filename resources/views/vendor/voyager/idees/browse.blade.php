145e13188ec467b7ae4bae9b7b8f2a29faea27c4
Cassius-P <49432067+Cassius-P@users.noreply.github.com>
2020-11-03 21:07:25 +0100

    Admin Voyager Views Ideas

--- resources/views/vendor/voyager/idees/browse.blade.php
+++ resources/views/vendor/voyager/idees/browse.blade.php
@@ -10,7 +10,6 @@
                 <div class="col-md-12">
                     <div class="panel panel-bordered">
                         <div class="panel-body">
-                            yeeeet
                             @if ($isServerSide)
                                 <form method="get" class="form-search">
                                     <div id="search-input">
@@ -257,6 +256,41 @@
         </div>
     @stop
 @else
+
+@section('page_header')
+    <div class="container-fluid">
+        <h1 class="page-title">
+            <i class="{{ $dataType->icon }}"></i> {{ $dataType->getTranslatedAttribute('display_name_plural') }}
+        </h1>
+        @can('add', app($dataType->model_name))
+            <a href="{{ route('voyager.'.$dataType->slug.'.create') }}" class="btn btn-success btn-add-new">
+                <i class="voyager-plus"></i> <span>{{ __('voyager::generic.add_new') }}</span>
+            </a>
+        @endcan
+        @can('delete', app($dataType->model_name))
+            @include('voyager::partials.bulk-delete')
+        @endcan
+        @can('edit', app($dataType->model_name))
+            @if(isset($dataType->order_column) && isset($dataType->order_display_column))
+                <a href="{{ route('voyager.'.$dataType->slug.'.order') }}" class="btn btn-primary btn-add-new">
+                    <i class="voyager-list"></i> <span>{{ __('voyager::bread.order') }}</span>
+                </a>
+            @endif
+        @endcan
+        @can('delete', app($dataType->model_name))
+            @if($usesSoftDeletes)
+                <input type="checkbox" @if ($showSoftDeleted) checked @endif id="show_soft_deletes" data-toggle="toggle" data-on="{{ __('voyager::bread.soft_deletes_off') }}" data-off="{{ __('voyager::bread.soft_deletes_on') }}">
+            @endif
+        @endcan
+        @foreach($actions as $action)
+            @if (method_exists($action, 'massAction'))
+                @include('voyager::bread.partials.actions', ['action' => $action, 'data' => null])
+            @endif
+        @endforeach
+        @include('voyager::multilingual.language-selector')
+    </div>
+@stop
+
     @section('content')
     <div class="page-content browse container-fluid">
         @include('voyager::alerts')
