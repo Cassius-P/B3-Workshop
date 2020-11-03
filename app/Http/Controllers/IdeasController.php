790983dbf1bf1b17d3f93a76f07b07884c79ecb5
Cassius-P <49432067+Cassius-P@users.noreply.github.com>
2020-11-03 19:15:36 +0100

    Joins Update

--- app/Http/Controllers/IdeasController.php
+++ app/Http/Controllers/IdeasController.php
@@ -19,17 +19,7 @@ class IdeasController extends Controller
             $id=$category->id;
             $ideas= DB::table('categories_idea')->where('categories_idea.categories_id', '=', $id)->join('idees', 'categories_idea.ideas_id', '=', 'idees.id')->where('idees.statut', '=', "1")->rightJoin("votes", function ($join){
                 $join->on("idees.id", "=", "votes.idea_id")->count();
-            })->leftJoin(
-
-                'users', 'idees.user_id', '=', 'users.id')->select(
-                    'idees.id',
-                    'idees.title',
-                    'idees.description',
-                    'idees.user_id',
-                    'idees.category_id',
-                    'idees.statut',
-                    'idees.upvotes',
-                    'users.name')->get();
+            })->get();
             $title = $category->title;
             $affichage[$title] = $ideas;
         }
