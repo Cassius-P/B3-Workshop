7b67f3b45c9aa13e7a964374fd1f405d7633e970
Cassius-P <49432067+Cassius-P@users.noreply.github.com>
2020-11-03 18:26:57 +0100

    Yann mange tes morts

--- resources/views/yann.blade.php
+++ resources/views/yann.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    {{$affichage}}
+                    {{print($affichage)}}
                 </div>
             </div>
         </div>
