fd207187b77ca20609d233e9c266b958df7bee69
ryry <lepescheuxryan@gmail.com>
2020-11-03 11:15:07 +0100

    added assets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="jumbotron">
-                    <h1 class="display-4">Hello, world!</h1>
-                    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                    <hr class="my-4">
-                    <p>Jordan mange mon manche</p>
-                    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-                </div>
+                <img src={{ asset('assets/banner.png') }} class="img-fluid" alt="banner">
             </div>
         </div>
     </div>
