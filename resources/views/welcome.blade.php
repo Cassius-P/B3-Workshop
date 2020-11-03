df83b06851577a6e2a49e748e07b891195ad6ac6
ryry <lepescheuxryan@gmail.com>
2020-11-03 14:20:44 +0100

    added some views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,37 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <img src={{ asset('assets/banner.png') }} class="img-fluid" alt="banner">
+                <img src={{ asset('assets/banner.png') }} class="mw-100" alt="banner">
+            </div>
+        </div>
+        <div class="container">
+            <div class="pt-5">
+                <div class="row">
+                    <div class="col-sm">
+                        <div class="card" style="width: 18rem; height: 18rem">
+                            <div class="card-body text-center">
+                              <h5 class="card-title">Titre 1</h5>
+                              <p class="card-text">{{ \Request::route()->getName() }}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body text-center">
+                              <h5 class="card-title">Titre 2</h5>
+                              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body text-center">
+                              <h5 class="card-title">Titre 3</h5>
+                              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
