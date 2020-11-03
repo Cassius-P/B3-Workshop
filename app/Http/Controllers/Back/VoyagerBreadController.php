eee468d5c37f2a2d20cc7f974f3b13f730ca2450
Cassius-P <49432067+Cassius-P@users.noreply.github.com>
2020-11-03 22:57:48 +0100

    Ideas Controller

--- app/Http/Controllers/Back/VoyagerBreadController.php
+++ app/Http/Controllers/Back/VoyagerBreadController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Back;
 
+use http\Env\Request;
 use TCG\Voyager\Http\Controllers\VoyagerBreadController as BaseVoyagerBreadController;
 
 class VoyagerBreadController extends BaseVoyagerBreadController
 {
-    //
+    public function index(Request $request){
+        //
+    }
 }
