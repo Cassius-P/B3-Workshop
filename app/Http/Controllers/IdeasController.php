11f19f4aa9bdd481d305982d01e399cd6ab6cc8d
Cassius-P <49432067+Cassius-P@users.noreply.github.com>
2020-11-03 18:58:20 +0100

    Joins Update

--- app/Http/Controllers/IdeasController.php
+++ app/Http/Controllers/IdeasController.php
@@ -27,8 +27,8 @@ class IdeasController extends Controller
                     'idees.statut',
                     'idees.upvotes',
                     'users.name')->get();
-            $titles = $category->title;
-            $affichage[$titles] = $ideas;
+            $title = $category->title;
+            $affichage[$title] = $ideas;
         }
 
         return view('yann', [ 'affichage' => $affichage]);
