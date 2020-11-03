26757ab2689c029ac84cefaba2b091199717cfba
Ziirkov <55087514+Ziirkov@users.noreply.github.com>
2020-11-03 14:28:05 +0100

    img

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <img src={{ asset('assets/banner.png') }} class="img-fluid" alt="banner">
+                <img src="{{ asset('assets/banner.png') }}" class="img-fluid" alt="banner">
             </div>
         </div>
     </div>
