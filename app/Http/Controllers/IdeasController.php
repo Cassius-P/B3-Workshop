f7c2fc3e8d5fd424545115da0040ec53b486c0f1
Cassius-P <49432067+Cassius-P@users.noreply.github.com>
2020-11-03 18:58:54 +0100

    Joins Update

--- app/Http/Controllers/IdeasController.php
+++ app/Http/Controllers/IdeasController.php
@@ -17,7 +17,7 @@ class IdeasController extends Controller
         $affichage = array();
         foreach ($categories as $category){
             $id=$category->id;
-            $ideas= DB::table('categories_idea')->where('categories_idea.categories_id', '=', $id)->join('idees', 'categories_idea.ideas_id', '=', 'idees.id')->where('idees.statut', '=', 1)->joinSub(
+            $ideas= DB::table('categories_idea')->where('categories_idea.categories_id', '=', $id)->join('idees', 'categories_idea.ideas_id', '=', 'idees.id')->where('idees.statut', '=', 1)->join(
                 'users', 'idees.user_id', '=', 'users.id')->select(
                     'idees.id',
                     'idees.title',
