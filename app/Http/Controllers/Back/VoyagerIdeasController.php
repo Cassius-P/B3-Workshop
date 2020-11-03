49a38fe0be4204bcb70fe722594319f81be9a81a
Cassius-P <49432067+Cassius-P@users.noreply.github.com>
2020-11-03 22:51:31 +0100

    Ideas Controller

--- app/Http/Controllers/Back/VoyagerIdeasController.php
+++ app/Http/Controllers/Back/VoyagerIdeasController.php
@@ -3,7 +3,35 @@
 namespace App\Http\Controllers\Back;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class VoyagerIdeasController extends VoyagerBreadController
 {
+    public function index(Request $request){
+        $slug = $this->getSlug($request);
+
+        $dataType = Voyager::model('DataType')->where('slug', '=', $slug);
+
+        $this->authorize('browse', app($dataType->model_name));
+
+        $getter = $dataType->server_side ? 'paginate' :  'get';
+
+        $orderBy = $request->get('oreder_by');
+        $sortOrder = $request->get('sort_order', null);
+
+        $isServerSide = isset($dataType->server_side) && $dataType->server_side;
+        $view = 'voyager::bread.browse';
+
+        if ( view()->exists("voyager::$slug.browse")){
+            $view = "voyager::$slug.browse";
+        }
+
+        return Voyager::view($view, compact(
+           'dataType',
+            'orderBy',
+            'sortOrder',
+            'isServerSide'
+        ));
+    }
 }
