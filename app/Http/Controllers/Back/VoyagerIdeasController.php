243cf84c9c25834f6fa59868b270ca8fd73e3f95
Cassius-P <49432067+Cassius-P@users.noreply.github.com>
2020-11-03 22:56:22 +0100

    Ideas Controller

--- app/Http/Controllers/Back/VoyagerIdeasController.php
+++ app/Http/Controllers/Back/VoyagerIdeasController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Back;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Back\VoyagerBreadController as BaseVoyagerBreadController;
 
 
 class VoyagerIdeasController extends VoyagerBreadController
