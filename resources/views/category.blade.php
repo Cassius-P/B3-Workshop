5c75b4be2d5785f6e5a84546f8a00cf00bec6807
Cassius-P <49432067+Cassius-P@users.noreply.github.com>
2020-11-04 16:06:30 +0100

    IdeaController.php

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -13,10 +13,9 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-9 mx-auto">
-<<<<<<< Updated upstream
+
                             <h2 class="text-center">A propos</h2>
-=======
->>>>>>> Stashed changes
+
                             <p class="lead">Voici une pésentation de ce que peut contenir éventuellement la descirption d'une catégorie :</p>
                             <p> Prenons l'exemple d'une catégorie au hasard comme par exemple l'amélioration d'espace verts
                                 On pourrait faire passer un arrêté municipal qui nous permettrait de dire que jeter un papier par terre est passible d'une amende de 35€
@@ -58,8 +57,8 @@
                 </div>
             </div>
         </div>
-    </div>  
+    </div>
 </div>
 
-    
+
 @endsection
