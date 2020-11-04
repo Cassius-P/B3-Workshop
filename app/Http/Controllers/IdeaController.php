4b0074c35aaea4670765c31cccf456fd7a823a70
keita23 <73839041+keita23@users.noreply.github.com>
2020-11-04 15:04:27 +0100

    idea

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -15,7 +15,7 @@ class IdeaController extends Controller
             return false;
         }else{          
             
-            $idee = DB::table('categories_idea')->where('categories_idea.idee_id', '=', $idea)->join('categories', 'categories_idea.categories_id', '=', 'categories.id')->leftJoin(
+            $idee = DB::table('categories_idea')->where('categories_idea.ideas_id', '=', $idea)->join('categories', 'categories_idea.categories_id', '=', 'categories.id')->leftJoin(
                 'idees', 'categories_idea.ideas_id', '=', 'idees.id')->where('idees.id', '=', $idea)->select(
                 'idees.id',
                 'idees.title',
