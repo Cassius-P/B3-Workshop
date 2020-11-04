f91e00104ff1a55553160e7a70320cfb79391c87
keita23 <73839041+keita23@users.noreply.github.com>
2020-11-04 12:39:26 +0100

    dev

--- app/Http/Controllers/XHRController.php
+++ app/Http/Controllers/XHRController.php
@@ -4,7 +4,56 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class XHRController extends Controller
-{
-    //
+class XHRController extends Controller{
+
+    public function newIdea(Request $request){
+        $sent = json_decode($request->get('info'));
+        if($sent){
+            $module = $sent->module;
+            if($module === 'idea'){
+                $message = insertIdea($sent);
+            }elseif($module === 'like'){
+                $message = insertLike($sent);
+            }
+            return response()->json(['message' => $message], 200);
+        }else{
+            return response()->json(['message' => 'error'], 200);
+        }
+    }
+
+    private function insertIdea($sent){
+        $titre = $sent->titre;
+        $desc = $sent->desc;
+        $user_id = $sent->user_id;
+
+        try 
+        { 
+            DB::table('idees')->insertGetId(
+                ['title' => $titre, 'description' => $desc, 'user_id' => $user_id]
+            );
+
+            return 'ok';
+        } 
+        catch(\Illuminate\Database\QueryException $e){ 
+            return $e->getMessage(); 
+        } 
+        
+    }
+
+    private function insertLike($sent){
+        $prop_id = $sent->prop_id;
+        $user_id = $sent->user_id;
+
+        try { 
+            DB::table('votes')->insertGetId(
+                ['idea_id' => $prop_id, 'user_id' => $user_id]
+            );
+            return 'ok';
+        } 
+        catch(\Illuminate\Database\QueryException $e){ 
+             return $e->getMessage(); 
+        }
+
+        
+    }
 }
