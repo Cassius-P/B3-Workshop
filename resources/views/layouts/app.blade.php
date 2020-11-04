904ed9af78a332b83f7bf5b78520dbf208c8d6a4
Cassius-P <49432067+Cassius-P@users.noreply.github.com>
2020-11-04 19:32:35 +0100

    CSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,8 +86,8 @@
         @endif
     </div>
 </body>
-<script src="{{ asset('js/jquery.min.js') }}" type="text/javascript"></script>
-<script src="{{ asset('js/oc/owl.carousel.min.js') }}" type="text/javascript"></script>
-<script src="{{ asset('js/script.js')}}" type="text/javascript"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" ></script>
+<script type="text/javascript" src="{{ asset('js/oc/owl.carousel.min.js') }}" ></script>
+<script type="text/javascript" src="{{ asset('js/script.js')}}" ></script>
 
 </html>
