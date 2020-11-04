1cc144285993a6bc35b8353dfed5e9a2b726c97e
Cassius-P <49432067+Cassius-P@users.noreply.github.com>
2020-11-04 23:12:14 +0100

    CSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -88,7 +88,7 @@
     </div>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" defer></script>
     <script type="text/javascript" src="{{ asset('js/oc/owl.carousel.js') }}" defer></script>
-    <script type="text/javascript" src="{{ asset('js/stackedCard.min.js')}}"></script>
+    <script type="text/javascript" src="{{ asset('js/stackedCards.min.js')}}"></script>
     <script type="text/javascript" src="{{ asset('js/script.js')}}" defer></script>
 </body>
 
