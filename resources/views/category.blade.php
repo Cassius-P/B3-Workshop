d142cab221772748804637908a98a9aa0321184b
Cassius-P <49432067+Cassius-P@users.noreply.github.com>
2020-11-04 16:06:30 +0100

    IdeaController.php

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -3,16 +3,19 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="container-titre text-center">
+        <div class="col-md-9">
+            <div class="container-titre text-center border-custom">
                 <h1>CATEGORIE</h1>
                 <p class="lead">Sous-titre de la catégorie</p>
             </div>
             <section id="about">
+            <h2 class="text-center separator h-about">A propos</h2>
                 <div class="container">
                     <div class="row">
-                        <div class="col-lg-8 mx-auto">
+                        <div class="col-lg-9 mx-auto">
+
                             <h2 class="text-center">A propos</h2>
+
                             <p class="lead">Voici une pésentation de ce que peut contenir éventuellement la descirption d'une catégorie :</p>
                             <p> Prenons l'exemple d'une catégorie au hasard comme par exemple l'amélioration d'espace verts
                                 On pourrait faire passer un arrêté municipal qui nous permettrait de dire que jeter un papier par terre est passible d'une amende de 35€
@@ -23,6 +26,7 @@
                     </div>
                 </div>
             </section>
+            <h2 class="separator"> Les idées de cette catégorie </h2>
             <div class="row liste-carte">
                 <div class="col-md-6">
                     <div class="carte card mb-6">
@@ -47,14 +51,14 @@
                         <div class="card-body">
                             <h5 class="card-title">Titre de la deuxième idée</h5>
                             <p class="card-text">Une courte phrase pour illustrer la description de lidée dans la card.</p>
-                            <a href="" class="btn btn-outline-dark btn-sm">VOIR</a> 
+                            <a href="" class="btn btn-outline-dark btn-sm">VOIR</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>  
+    </div>
 </div>
 
-    
+
 @endsection
