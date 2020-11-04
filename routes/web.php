a0ecac37086c63f5e9ea7d7d0c1c2cfdc092dd23
Ziirkov <55087514+Ziirkov@users.noreply.github.com>
2020-11-04 15:33:41 +0100

    Idee + categories

--- routes/web.php
+++ routes/web.php
@@ -14,23 +14,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/category', function () {
     return view('category');
 });
 
+<<<<<<< Updated upstream
+Route::get('/category', function () {
+    return view('category');
+});
+=======
+
+
+>>>>>>> Stashed changes
+
 Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
+<<<<<<< Updated upstream
+=======
 
 
-Route::get('/idees', [App\Http\Controllers\IdeasController::class, 'getIdeas']);
-Route::get('/{x}', [App\Http\Controllers\CategoriesController::class, 'index']);
-Route::get('/{x}/{y}', [App\Http\Controllers\IdeaController::class, 'index']);
 
-Route::post('/xhr', [App\Http\Controllers\XHRController::class, 'newIdea']);
-
+>>>>>>> Stashed changes
