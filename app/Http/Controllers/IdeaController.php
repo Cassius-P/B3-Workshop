6595f030e9df60fc9aaeb2cbb841ba36b476c61e
keita23 <73839041+keita23@users.noreply.github.com>
2020-11-04 14:26:32 +0100

    idea

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class IdeaController extends Controller
 {
-    public function index(){
-        //
+    public function index($url){
+        return view('idea', ['url' => $url]);
     }
 }
