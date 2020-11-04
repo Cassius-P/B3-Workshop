025c4ccb5b7b52b9877c5057d9b6703361341aca
keita23 <73839041+keita23@users.noreply.github.com>
2020-11-04 15:01:17 +0100

    idea

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -14,14 +14,13 @@ class IdeaController extends Controller
             abort(404);
             return false;
         }else{          
-            //$ = Ideas::where('id', '=', $idea );
+            
             $idee = DB::table('categories_idea')->where('categories_idea.idee_id', '=', $idea)->join('categories', 'categories_idea.categories_id', '=', 'categories.id')->leftJoin(
                 'idees', 'categories_idea.ideas_id', '=', 'idees.id')->where('idees.id', '=', $idea)->select(
                 'idees.id',
                 'idees.title',
                 'idees.description',
                 'idees.user_id',
-                'idees.category_id',
                 'idees.statut',
                 'idees.upvotes',
                 'categories.slug')->get();
