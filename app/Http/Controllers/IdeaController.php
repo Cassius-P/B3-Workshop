db715b7b055514b7396dc82e9a458ebc831ddf08
Cassius-P <49432067+Cassius-P@users.noreply.github.com>
2020-11-04 16:32:22 +0100

    IdeaController

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -1,12 +1,42 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Categories;
+use Illuminate\Support\Facades\DB;
 
 use Illuminate\Http\Request;
 
 class IdeaController extends Controller
 {
-    public function index(){
-        //
+    public function index($category, $idea){
+        $cat = Categories::where('slug', '=', $category)->first();
+        if($cat == null){
+            abort(404);
+            return false;
+        }else{
+
+            $idee = DB::table('categories_idea')->where('categories_idea.ideas_id', '=', $idea)->join('categories', 'categories_idea.categories_id', '=', 'categories.id')->leftJoin(
+                'idees', 'categories_idea.ideas_id', '=', 'idees.id')->where('idees.id', '=', $idea)->select(
+                'idees.id',
+                'idees.title',
+                'idees.description',
+                'idees.user_id',
+                'idees.statut',
+                'categories.slug')->first();
+
+
+            if ($idee->statut == 0) {
+                abort(404);
+                return false;
+            }else{
+                $idee->vote = DB::table('votes')->where('idea_id', '=', $idea)->count();
+                $name = DB::table('users')->where('users.id', '=', $idee->user_id)->select('users.name')->first();
+                $idee->user = $name->name;
+
+                return view('idea', ['url' => $idee]);
+            }
+
+        }
+
     }
 }
