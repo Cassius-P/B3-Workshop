5eb18dab08e23da48295fe338d984dd2629afa5a
ryry <lepescheuxryan@gmail.com>
2020-11-05 17:22:31 +0100

    translated to fr

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,20 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Verifiez votre adresse email') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('Un lien de vérification vous a été envoyé sur votre adresse email') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    {{ __('Merci de vérifier le lien que nous avons envoyé à votre adresse email avant de continuer') }}
+                    {{ __('Si vous n'avez reçu aucun email') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('cliquez-ici pour renvoyer un mail de vérification') }}</button>.
                     </form>
                 </div>
             </div>
