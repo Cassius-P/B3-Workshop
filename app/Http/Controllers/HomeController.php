f9f213b620cb49680d46cda6c59055df5ed7ca37
Cassius-P <49432067+Cassius-P@users.noreply.github.com>
2020-11-05 12:20:30 +0100

    Home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,17 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $id = Auth::user()->id;
+        $liked = $this->getLikes($id);
+        $posted = $this->getPosted($id);
+        return view('home', ["liked" => $liked, "posted" => $posted]);
+    }
+
+    private function getLikes($id){
+        //
+    }
+
+    private function getPosted($id){
+        //
     }
 }
