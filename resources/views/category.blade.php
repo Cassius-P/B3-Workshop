89bd9b59df94279c507c144787a7e53b6683570c
Ziirkov <55087514+Ziirkov@users.noreply.github.com>
2020-11-05 12:04:54 +0100

    css category comme avant

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -6,33 +6,33 @@
 
         <div class="col-md-9">
             <div class="container-titre text-center border-custom">
-
                 <h1>{{$category->title}}</h1>
             </div>
+            <h2 class="text-center separator">À propos</h2>
             <section id="about">
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-9 mx-auto">
-                            <h2 class="text-center">A propos</h2>
-                            <p> {{$category->description}}
+                            
+                            <p>{{$category->description}}
                             </p>
                         </div>
                     </div>
                 </div>
+            <h2 class="text-center separator">Les idées de cette catégorie</h2>    
             </section>
             <div class="row liste-carte">
-
-                @foreach($ideas as $idea)
+            @foreach($ideas as $idea)
                     <div class="col-md-6">
                         <div class="carte card mb-6">
                             <div class="card-body">
-                                <h5 class="card-title">{{$idea->title}}</h5>
-                                <p class="card-text">{{$idea->description}}</p>
-                                <a href="/{{$category->slug}}/{{$idea->id}}" class="btn btn-outline-dark btn-sm">VOIR</a>
+                                <h5 class="card-title">Titre</h5>
+                                <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis cupiditate, doloribus vero quo quasi omnis illo earum magni magnam numquam hic odio quae tenetur nesciunt! Nihil impedit beatae fuga repellendus!</p>
+                                <a href="" class="btn btn-outline-dark btn-sm">VOIR</a>
                             </div>
                         </div>
                     </div>
-                @endforeach
+            @endforeach
 
             </div>
         </div>
