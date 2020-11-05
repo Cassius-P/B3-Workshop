0ff011f5021cfc82ea1d7faf15e08361725109a3
Cassius-P <49432067+Cassius-P@users.noreply.github.com>
2020-11-05 12:53:57 +0100

    Home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,10 +61,11 @@
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
-                                <a class="dropdown-item" href="{{ route('home') }}">
-                                    Mon compte
-                                </a>
+
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('home') }}">
+                                        Mon compte
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
