e545d5279e4f25a96e8c3004f47865d4e2e18cdb
Cassius-P <49432067+Cassius-P@users.noreply.github.com>
2020-11-05 11:56:08 +0100

    Nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,6 +44,9 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="/idees">Idées</a>
+                        </li>
                         @guest
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Se connecter') }}</a>
