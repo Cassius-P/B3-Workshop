69c3c34846cfce0dc98536cc76ecce57b081be13
Cassius-P <49432067+Cassius-P@users.noreply.github.com>
2020-11-05 19:14:03 +0100

    Home

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -2,7 +2,12 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="rox justify-content-center">
+        <div class="jumbotron my-5 bg-ambre" style="color:white;">
+            <h1 class="display-4" style="color:#f0c300">{{$category->title}}!</h1>
+            <hr class="my-4" style="background-color:var(--light)">
+            <p style="color:var(--light)">{{$idee->description}}</p>
+        </div>
 
         <div class="col-md-9">
             <div class="container-titre text-center border-custom">
@@ -13,13 +18,13 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-9 mx-auto">
-                            
+
                             <p>{{$category->description}}
                             </p>
                         </div>
                     </div>
                 </div>
-            <h2 class="text-center separator">Les idées de cette catégorie</h2>    
+            <h2 class="text-center separator">Les idées de cette catégorie</h2>
             </section>
             <div class="row liste-carte">
             @foreach($ideas as $idea)
