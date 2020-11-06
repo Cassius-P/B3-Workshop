aed97304ab4d0af71ed15cd2a2789d66b82768e7
Cassius-P <49432067+Cassius-P@users.noreply.github.com>
2020-11-06 03:02:55 +0100

    Homepage redesign

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,35 +6,43 @@
     <div class="row justify-content-center  my-5">
         <h1 class="list-group-item active col-sm-10 col-lg-12 col-md-10 b-shadow2">Vos idées :</h1>
         <div class="row container d-flex flex-wrap justify-content-between mt-2 mb-5">
-            @foreach($posted as $post)
-                <div class="col-lg-3 col-md-3 col-sm-12 card m-1 bg-transparent b-shadow px-0">
-                    <div class="card-header bg-ambre b-hover text-center">
-                        <h5 class="card-title c-white">{{$post->title}}</h5>
-                    </div>
-                    <div class="card-body pb-3">
-                        <p class="card-text" style="color:#473c38" >{{$post->description}}</p>
-                        <div class="row more-details-description">
-                            <a href="{{$post->slug}}/{{$post->id}}">Plus...</a>
+            @if(isEmpty($posted))
+                <h5>Vous n'avez pas proposé d'idées pour le moment</h5>
+            @else
+                @foreach($posted as $post)
+                    <div class="col-lg-3 col-md-3 col-sm-12 card m-1 bg-transparent b-shadow px-0">
+                        <div class="card-header bg-ambre b-hover text-center">
+                            <h5 class="card-title c-white">{{$post->title}}</h5>
+                        </div>
+                        <div class="card-body pb-3">
+                            <p class="card-text" style="color:#473c38" >{{$post->description}}</p>
+                            <div class="row more-details-description">
+                                <a href="{{$post->slug}}/{{$post->id}}">Plus...</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            @endif
         </div>
         <h1 class="list-group-item active col-sm-10 col-lg-12 col-md-10 b-shadow2">Vos likes :</h1>
         <div class="row container d-flex flex-wrap justify-content-between mt-2 mb-5">
-            @foreach($liked as $like)
-                <div class="col-lg-3 col-md-3 col-sm-12 card m-1 bg-transparent b-shadow px-0">
-                    <div class="card-header bg-ambre b-hover text-center">
-                        <h5 class="card-title c-white">{{$like->title}}</h5>
-                    </div>
-                    <div class="card-body">
-                        <p class="card-text" style="color:#473c38" >{{$like->description}}</p>
-                        <div class="row more-details-description">
-                            <a href="{{$like->slug}}/{{$like->id}}">Plus...</a>
+            @if(isEmpty($liked))
+                <h5>Vous n'avez pas proposé d'idées pour le moment</h5>
+            @else
+                @foreach($liked as $like)
+                    <div class="col-lg-3 col-md-3 col-sm-12 card m-1 bg-transparent b-shadow px-0">
+                        <div class="card-header bg-ambre b-hover text-center">
+                            <h5 class="card-title c-white">{{$like->title}}</h5>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text" style="color:#473c38" >{{$like->description}}</p>
+                            <div class="row more-details-description">
+                                <a href="{{$like->slug}}/{{$like->id}}">Plus...</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            @endif
         </div>
 
     </div>
