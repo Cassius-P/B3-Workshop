5466c6573b1d595c66f786f2a464dbdf8b4a9fed
Cassius-P <49432067+Cassius-P@users.noreply.github.com>
2020-11-06 02:26:22 +0100

    Homepage redesign

--- resources/views/idea.blade.php
+++ resources/views/idea.blade.php
@@ -18,7 +18,7 @@
                     <div class="card-footer bg-transparent border-marron d-flex justify-content-around align-items-center text-align-center">
                         <span style="color:white">{{$idee->vote}}<span>&nbsp;likes</span></span>
                         @if($liked == false)
-                            <button id="buttonLike" type="button" data-idea="{{$idee->id}}" data-user="{{$idee->user_id}}" class="btn bg-ambre btn-success border-ambre" style="color:white">
+                            <button id="buttonLike" type="button" data-idea="{{$idee->id}}" data-user="{{$user_id}}" class="btn bg-ambre btn-success border-ambre" style="color:white">
                                 <span class="btn-label"><i class="fa fa-heart" aria-hidden="true"></i></span>J'aime !
                             </button>
                         @else
